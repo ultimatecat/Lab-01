785344b81d1e9d81819f61e9d67cc1a0dd863266
ultimatecat <42696018+ultimatecat@users.noreply.github.com>
2018-10-19 14:12:45 +0700

    Update Lab 02.cpp

---
+++ Lab 02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<endl;
+	cout<<"\t* *\t*\t * * * *\t*\t*\t*\t     * *\n";
+	cout<<"\t*  *\t*\t*\t\t*\t*\t*\t    *   *\n";
+	cout<<"\t*   *\t*\t*\t\t* * * * *\t*\t   *     *\n";
+	cout<<"\t*    *\t*\t*   * * *\t*\t*\t*\t  * * * * *\n";
+	cout<<"\t*     *\t*\t*       *\t*\t*\t*\t *         *\n";
+	cout<<"\t*       * \t * * * *\t*\t*\t*\t*           *\n";
+	return 0;
+}
